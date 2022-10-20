06114f9f7f68ce905e79cbd0ec5e727fc579d3e2
bidduthosen <mdbidduthosen34@gmail.com>
2022-10-21 00:27:05 +0600

    change text

--- src/components/Shipping/Shipping.js
+++ src/components/Shipping/Shipping.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Shipping = () => {
     return (
         <div>
-            <h1>shipping</h1>
+            <h1>shipping.</h1>
             <h1>Coming soon....</h1>
         </div>
     );
